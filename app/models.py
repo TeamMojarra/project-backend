78e00e310b55c3d2d5c186c6eeeb69166605d117
Layo Andres Moreno <lamorenoco@unal.edu.co>
2026-05-19 22:47:07 -0500

    Delete app/models.py

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-
-class Greeting(Base):
-    __tablename__ = "greetings"
-
-    id = Column(Integer, primary_key=True, index=True)
-    content = Column(String, nullable=False)
